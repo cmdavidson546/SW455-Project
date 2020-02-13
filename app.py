74d850aebe0db00c7ce51b568e184a52b9297d4c
Christopher Davidson <christopherdavidson@Christophers-MacBook-Pro.local>
2020-02-12 22:53:42 -0500

    v 2.3 bug-fix-4

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ __author__ = "christopherdavidson"
 
 from flask import Flask, render_template, request, session, jsonify, make_response
 from common.database import Database
+from models.meeting import Meeting
 from models.admin import Admin
 from models.client import Client
 from models.user import User
